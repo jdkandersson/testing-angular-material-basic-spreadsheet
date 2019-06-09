2b5f7419a76b933fcdf06652af6233838c2fff11
jdkandersson <51036209+jdkandersson@users.noreply.github.com>
2019-06-09 12:05:51 +1000

    Update order.component.spec.ts

--- order.component.spec.ts
+++ order.component.spec.ts
@@ -5,48 +5,48 @@ import { OrderComponent } from './order.component';
 import { DebugElement } from '@angular/core';
 
 describe('OrderComponent', () => {
-    let fixture: ComponentFixture<OrderComponent>;
-    let component: OrderComponent;
-    let debugElement: DebugElement;
+  let fixture: ComponentFixture<OrderComponent>;
+  let component: OrderComponent;
+  let debugElement: DebugElement;
 
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            declarations: [OrderComponent]
-        });
-
-        fixture = TestBed.createComponent(OrderComponent);
-        component = fixture.componentInstance;
-        debugElement = fixture.debugElement;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [OrderComponent]
     });
 
-    it('should create', () => {
-        fixture.detectChanges();
-        expect(component).toBeTruthy();
+    fixture = TestBed.createComponent(OrderComponent);
+    component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
+  });
+
+  it('should create', () => {
+    fixture.detectChanges();
+    expect(component).toBeTruthy();
+  });
+
+  describe('order null', () => {
+    it('should display empty template.', () => {
+      // Setting order to null
+      component.order = null;
+
+      // Triggering change detection
+      fixture.detectChanges();
+
+      // Checking template
+      expect(debugElement.nativeElement.textContent).toEqual('');
     });
+  });
 
-    describe('order null', () => {
-        it('should display empty template.', () => {
-            // Setting order to null
-            component.order = null;
+  describe('order not null', () => {
+    it('should display order value formatted as number.', () => {
+      // Setting order to null
+      component.order = {date: '2000-01-01', product: 'product 1', value: 1000};
 
-            // Triggering change detection
-            fixture.detectChanges();
+      // Triggering change detection
+      fixture.detectChanges();
 
-            // Checking template
-            expect(debugElement.nativeElement.textContent).toEqual('');
-        });
+      // Checking template
+      expect(debugElement.nativeElement.textContent).toEqual('1,000');
     });
-
-    describe('order not null', () => {
-        it('should display order value formatted as number.', () => {
-            // Setting order to null
-            component.order = {date: '2000-01-01', product: 'product 1', value: 1000};
-
-            // Triggering change detection
-            fixture.detectChanges();
-
-            // Checking template
-            expect(debugElement.nativeElement.textContent).toEqual('1,000');
-        });
-    });
+  });
 });
